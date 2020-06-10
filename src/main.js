cea4eb38220866d7d5fe993a4f347ea7f9c56b67
Nathan Altice <naltice@ucsc.edu>
2020-06-09 23:09:04 -0700

    initial commit - FSM working

---
+++ src/main.js
@@ -0,0 +1,23 @@
+// Nathan Altice
+// Created: 6/9/20
+// Finite State Machines
+// Example 1 adapted from mkelly.me/blog/phaser-finite-state-machine
+
+'use strict';
+
+const config = {
+    type: Phaser.AUTO,
+    width: 400,
+    height: 300,
+    pixelArt: true,
+    zoom: 2,
+    physics: {
+        default: "arcade"
+    },
+    scene: [ FSMExample ]
+};
+
+// define game
+const game = new Phaser.Game(config);
+
+// global stuff
