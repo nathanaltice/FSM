3203389598e757c6bcad6e16e11ddda2b4bf5458
Nathan Altice <naltice@ucsc.edu>
2023-10-29 19:09:38 -0700

    improved hitbox; removed semicolons; debug toggle

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 // Nathan Altice
 // Created: 6/9/20
-// Updated: 4/25/23 (Phaser 3.60)
+// Updated: 10/29/23 (Phaser 3.60)
 // Finite State Machines
 // CharacterFSM example adapted from mkelly.me/blog/phaser-finite-state-machine
 // Refactored for Hero prefab, detangled scene code from hero code, added 'hurt' state
 
 // DE-DANGER
-'use strict';
+'use strict'
 
 const config = {
     parent: 'phaser-game',  // for info text
@@ -14,11 +14,14 @@ const config = {
     width: 400,
     height: 300,
     pixelArt: true,
-    zoom: 2,
+    zoom: 3,
     physics: {
-        default: "arcade"
+        default: "arcade",
+        arcade: {
+            debug: true
+        }
     },
     scene: [ CharacterFSM ]
-};
+}
 
-const game = new Phaser.Game(config);
+const game = new Phaser.Game(config)
